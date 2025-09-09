ee8633c0f7091de92b3333cafbb96b8b8767b03e
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-09 19:40:56 +0800

    Day02_Print.java

---
+++ Day02_Print.java
@@ -0,0 +1,7 @@
+public class Day02_Print{
+    public static void main(String[] args) {
+        System.out.print("Halo Nama Saya :");
+        System.out.println(" Habib");
+        System.out.printf("Saya dari kelas : %c", 'B');
+    }
+}
