51ec3646cd310532d2bd951e0674809fe3350ed7
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-10 19:46:55 +0800

    Day03_Comentar.java
    yooo

---
+++ Day03_Comentar.java
@@ -0,0 +1,14 @@
+public class Day03_Comentar {
+    public static void main(String[] args) {
+        // Menampilkan teks ke konsol
+        System.out.println("Penjumlahan");
+
+        // Deklarasi variabel untuk menyimpan angka
+        int angka1 = 10; // Angka pertama
+        int angka2 = 20; // Angka kedua
+        int hasil = angka1 + angka2; // Menjumlahkan angka1 dan angka2
+
+        // Menampilkan hasil penjumlahan ke konsol
+        System.out.println("Hasil dari " + angka1 + " + " + angka2 + " = " + hasil);
+    }   
+}
