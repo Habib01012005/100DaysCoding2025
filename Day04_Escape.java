7fea2b7c2ecf84f2d404de7c023b0aaa195eb35f
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-11 21:12:51 +0800

    Day04_Escape.java

---
+++ Day04_Escape.java
@@ -0,0 +1,9 @@
+public class Day04_Escape {
+    public static void main(String[] args) {
+        System.out.println("Halo nama saya, \"Habib\" salam kenal");
+        System.out.println("saya tinggal di :\nLembang");
+        System.out.println("NIM saya \t : D0223017");
+        System.out.println("Saya suka makan mie ayam \\ bakso");
+        System.out.println("\"Terima Kasih \"");
+    }
+}
