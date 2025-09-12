17fbf9e3cd328e8ed7f1ac3e93dec2ca8e529efe
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-12 15:29:14 +0800

    Day05_BilanganBulat.java

---
+++ Day05_BilanganBulat.java
@@ -0,0 +1,13 @@
+public class Day05_BilanganBulat {
+    public static void main(String[] args) {
+        byte angka1 = 10;
+        short angka2 = 20;
+        int angka3 = 30;
+        long angka4 = 40L;
+
+        System.out.println("Nilai byte  : " + angka1);
+        System.out.println("Nilai short : " + angka2);
+        System.out.println("Nilai int   : " + angka3);
+        System.out.println("Nilai long  : " + angka4);
+    }
+}
