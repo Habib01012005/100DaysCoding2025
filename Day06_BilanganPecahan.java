d2384fbdeaa45fa91036f402874c530b244a67b7
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-13 20:12:40 +0800

    Day06_BilanganPecahan.java
    yo i

---
+++ Day06_BilanganPecahan.java
@@ -0,0 +1,9 @@
+public class Day06_BilanganPecahan {
+    public static void main(String[] args) {
+        double angka1 = 10.5;
+        float angka2 = 20.5f;
+
+        System.out.println("Nilai double : " + angka1);
+        System.out.println("Nilai float  : " + angka2);
+    }
+}
