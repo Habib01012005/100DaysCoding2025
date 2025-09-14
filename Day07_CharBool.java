56fc25e8ca7bbe0ce957dd3fa48d064e235fc9e5
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-14 18:48:39 +0800

    Day07_CharBool.java
    seperti biasa

---
+++ Day07_CharBool.java
@@ -0,0 +1,9 @@
+public class Day07_CharBool {
+    public static void main(String[] args) {
+        char kelas = 'B';
+        boolean Benar = true;
+
+        System.out.println("Saya dari Kelas : " + kelas);
+        System.out.println("Kamu Dari Kelas B? " + Benar);
+    }
+}
