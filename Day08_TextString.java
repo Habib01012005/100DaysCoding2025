cbd5012da03648131155a9b67b70d46fbad0100d
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-15 20:35:10 +0800

    Day08_TextString.java
    go

---
+++ Day08_TextString.java
@@ -0,0 +1,12 @@
+public class Day08_TextString {
+    public static void main(String[] args) {
+
+        String nama = "Habib";
+        String alamat = "Lembang";
+
+        System.out.println("Nama Saya : " + nama);
+        System.out.println("Alamat Saya : " + alamat);
+
+        System.out.println("Nama Saya : " + nama + ", Alamat Saya : " + alamat);
+    }
+}
