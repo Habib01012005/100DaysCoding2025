2943d09fa793a965466b49eb74a1603aa99824a7
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-18 22:58:59 +0800

    Day11_Scanner.java
    yoo

---
+++ Day11_Scanner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Day11_Scanner {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Nama Saya : ");
+    String nama = input.nextLine();
+    System.out.println("Nama Saya Adalah : "+nama);
+
+    }
+}
