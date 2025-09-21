e43178ea8c3189579a608bbe43f2cbbf1d3cbd6f
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-21 20:13:22 +0800

    Day14_StringPrimitif.java
    iksooo

---
+++ Day14_StringPrimitif.java
@@ -0,0 +1,17 @@
+public class Day14_StringPrimitif {
+    public static void main(String[] args) {
+        
+        String a = "12";
+        String b = "34";
+        String c = "12.34";
+
+        int d = Integer.parseInt(a);
+        byte e = Byte.parseByte(b);
+        double f = Double.parseDouble(c);
+
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+    
+}
