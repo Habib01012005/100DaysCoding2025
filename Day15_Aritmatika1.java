988832608b73f84a12261a3acce9aa4f39b1f342
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-22 21:12:58 +0800

    Day15_Aritmatika1.java
    yoyoyoyo

---
+++ Day15_Aritmatika1.java
@@ -0,0 +1,14 @@
+public class Day15_Aritmatika1 {
+    public static void main(String[] args) {
+        int a, b, c, d;
+        
+        a = 123;
+        b = 456;
+
+        c = a + b;
+        d = b - a;
+
+        System.out.println("Hasil Penjumlahan: " + c);
+        System.out.println("Hasil Pengurangan: " + d);
+    }
+}
