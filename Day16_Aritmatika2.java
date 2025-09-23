7fdbb1019691f981824752748bc47d8a659bcb88
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-23 19:43:17 +0800

    Create Day16_Aritmatika2.java
    perkalian dan pembagian

---
+++ Day16_Aritmatika2.java
@@ -0,0 +1,14 @@
+public class Day16_Aritmatika2 {
+    public static void main(String[] args) {
+        int a, b, c, d;
+        a = 12;
+        b = 4;
+
+        c = a * b;
+        d = a / b;
+
+        System.out.println("Hasil Perkalian: " + c);
+        System.out.println("Hasil Pembagian: " + d);
+
+    }
+}
