c13cd40ee2c37a228c50e686032a4c43b3e1f04c
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-24 21:52:33 +0800

    Day17_Aritmatika3.java
    dibantai

---
+++ Day17_Aritmatika3.java
@@ -0,0 +1,11 @@
+public class Day17_Aritmatika3 {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 20;
+        b = 3;
+
+        c = a % b;
+
+        System.out.println("Hasil Modulus : "+ c);
+    }
+}
