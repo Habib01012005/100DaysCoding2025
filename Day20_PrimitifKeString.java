c4dbde0db2cabdb914ba667a7be44b755db8ebca
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-27 20:32:12 +0800

    Day20_PrimitifKeString.java
    yokoso

---
+++ Day20_PrimitifKeString.java
@@ -0,0 +1,17 @@
+public class Day20_PrimitifKeString {
+    public static void main(String[] args) {
+
+        int a = 12;
+        double b = 45.6;
+        short c = 34;
+
+        String d = Double.toString(a);
+        String e = b + "";
+        String f = String.valueOf(c);
+        
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
+}
