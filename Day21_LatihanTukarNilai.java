bcfb0d18994283b9305cc7a3d4cfd9603f21ea81
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-28 22:22:31 +0800

    Day21_LatihanTukarNilai.java
    hohoho

---
+++ Day21_LatihanTukarNilai.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day21_LatihanTukarNilai {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int angka1 = sc.nextInt();
+        int angka2 = sc.nextInt();
+
+        angka1 = angka1 + angka2;
+        angka2 = angka1 - angka2;
+        angka1 = angka1 - angka2;
+        
+        System.out.println(angka1+" "+angka2);
+    }
+}
