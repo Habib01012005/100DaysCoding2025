4b91487b0f190b288ad99d64e4743f04445efde8
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-29 20:26:22 +0800

    Day22_LuasPersegi.java
    hoo

---
+++ Day22_LuasPersegi.java
@@ -0,0 +1,10 @@
+public class Day22_LuasPersegi {
+    public static void main(String[] args) {
+        int sisi = 8;
+
+        int luas = sisi * sisi;
+
+        System.out.println(sisi);
+        System.out.println(luas);
+    }
+}
