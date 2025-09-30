3f80f90c3e7209a840a614deaa0c43be1514e802
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-09-30 21:41:21 +0800

    Day23_LuasPersegiPanjang.java
    yokoso

---
+++ Day23_LuasPersegiPanjang.java
@@ -0,0 +1,10 @@
+public class Day23_LuasPersegiPanjang {
+    public static void main(String[] args) {
+        int panjang = 100;
+        int lebar = 200;
+
+        int luas = panjang * lebar;
+
+        System.out.println(luas);
+    }
+}
