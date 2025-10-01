3c6d50255f96d5bdfc927e1f445c02fb2491424b
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-10-01 22:46:21 +0800

    Day24_LuasLingkaran.java
    hooo

---
+++ Day24_LuasLingkaran.java
@@ -0,0 +1,12 @@
+public class Day24_LuasLingkaran {
+    public static void main(String[] args) {
+        
+        double phi = Math.PI;
+        int r = 8;
+
+        double luas = phi * r * r;
+
+        System.out.printf("Luas Lingkaran = %.2f", luas);
+
+    }
+}
