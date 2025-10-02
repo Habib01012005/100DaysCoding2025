e90348b5a809e95a78634c485e8ec933c989fb72
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-10-02 22:31:29 +0800

    Day25_IncreDecre.java
    hooo

---
+++ Day25_IncreDecre.java
@@ -0,0 +1,11 @@
+public class Day25_IncreDecre {
+    public static void main(String[] args) {
+        int a = 20;
+
+        a++;
+        System.out.println("Increment a = "+ a);
+
+        a--;
+        System.out.println("Decrement a = "+ a);
+    }
+}
