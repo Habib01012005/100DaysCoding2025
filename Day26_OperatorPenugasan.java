2fe1c87a435ee53f966db3a1abffe716900fd89b
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-10-03 19:32:14 +0800

    Day26_OperatorPenugasan.java
    yoooo

---
+++ Day26_OperatorPenugasan.java
@@ -0,0 +1,22 @@
+public class Day26_OperatorPenugasan {
+    public static void main(String[] args) {
+        int angka1 = 20;
+        int angka2 = 5;
+
+        angka1 += angka2;
+        System.out.println(angka1);
+
+        angka1 -= angka2;
+        System.out.println(angka1);
+
+        angka1 *= angka2;
+        System.out.println(angka1);
+
+        angka1 /= angka2;
+        System.out.println(angka1);
+
+        angka1 %= angka2;
+        System.out.println(angka1);
+        
+    }
+}
