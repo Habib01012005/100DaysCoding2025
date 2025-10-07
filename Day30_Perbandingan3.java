74cb68e6e21298b42096f26c6e349d1ca0e55858
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-10-07 19:08:25 +0800

    Day30_Perbandingan3.java
    yohohohoho

---
+++ Day30_Perbandingan3.java
@@ -0,0 +1,13 @@
+public class Day30_Perbandingan3 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+        int c = 20;
+
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a >= c);
+        System.out.println(a <= c);
+
+    }
+}
