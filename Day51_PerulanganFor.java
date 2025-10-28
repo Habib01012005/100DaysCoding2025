26fac651892e9ae43acb9fd2ceddf861e3ab04fc
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-10-28 23:43:48 +0800

    Create Day51_PerulanganFor.java

---
+++ Day51_PerulanganFor.java
@@ -0,0 +1,7 @@
+public class Day51_PerulanganFor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Habib");
+        }
+    }
+}
