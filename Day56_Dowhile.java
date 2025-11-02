d4c0a909873494c9a8a8fc7fdbbb8ae3ffd85a0c
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-11-02 23:09:29 +0800

    Create  Day56_Dowhile.java

---
+++ Day56_Dowhile.java
@@ -0,0 +1,9 @@
+public class Day56_Dowhile {
+    public static void main(String[] args) {
+        int angka = 1;
+        do {
+            System.out.println("Angka Ke-"+angka);
+            angka++;
+        } while (angka <= 7);
+    }
+}
