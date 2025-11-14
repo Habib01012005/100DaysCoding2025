729990f59dcb467de06e743cf95a9863cfc80ea3
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-11-14 22:37:11 +0800

    Create Day67_PolaPersegi.java

---
+++ Day67_PolaPersegi.java
@@ -0,0 +1,10 @@
+public class Day67_PolaPersegi {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("o ");
+            }
+            System.out.println("o ");
+        }
+    }
+}
