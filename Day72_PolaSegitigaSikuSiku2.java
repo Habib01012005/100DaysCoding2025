ec11eeef5ecf812760012578b45ecc22c540ddca
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-11-18 21:39:52 +0800

    Create Day72_PolaSegitigaSikuSiku2.java

---
+++ Day72_PolaSegitigaSikuSiku2.java
@@ -0,0 +1,10 @@
+public class Day72_PolaSegitigaSikuSiku2 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("O ");
+            }
+            System.out.println("O ");
+        }
+    }
+}
