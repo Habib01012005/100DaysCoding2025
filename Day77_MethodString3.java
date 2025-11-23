098fdc9a71d430c969005578a78d5a7feda92fc4
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-11-23 23:18:44 +0800

    Create Day77_MethodString3.java

---
+++ Day77_MethodString3.java
@@ -0,0 +1,9 @@
+public class Day77_MethodString3 {
+    public static void main(String[] args) {
+        String teks = " Saya Habib ";
+
+        System.out.println(teks.substring(0, 6));
+        System.out.println(teks.replace("Saya", "Hay"));
+        System.out.println(teks.trim());
+    }
+}
