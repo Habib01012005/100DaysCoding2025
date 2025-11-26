b9144fe96f0bb5b2e183c8530d378c8e708815d0
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-11-26 22:24:12 +0800

    Day80_Array1D.java

---
+++ Day80_Array1D.java
@@ -0,0 +1,9 @@
+public class Day80_Array1D {
+    public static void main(String[] args) {
+        int angka [] = {10, 20, 30};
+
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Index ke-"+i+" = "+angka[i]);
+        }
+    }
+}
