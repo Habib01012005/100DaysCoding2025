d74c69d62c900bd52a087bd4e0caa8e52f580ac9
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-11-27 23:07:57 +0800

    Create Day81_AksesIndexArray.java

---
+++ Day81_AksesIndexArray.java
@@ -0,0 +1,9 @@
+public class Day81_AksesIndexArray {
+    public static void main(String[] args) {
+        int angka [] = {10, 20, 30};
+        
+        System.out.println(angka[1]);
+        System.out.println(angka[0]);
+        System.out.println(angka[2]);
+    }
+}
