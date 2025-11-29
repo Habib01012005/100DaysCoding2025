c5cc57ffc9670db6ba4c91f74eab235b380508ce
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-11-29 12:53:48 +0800

    Create Day83_PenjumlahanElemenArray.java

---
+++ Day83_PenjumlahanElemenArray.java
@@ -0,0 +1,10 @@
+public class Day83_PenjumlahanElemenArray {
+    public static void main(String[] args) {
+        int [] angka = {5 , 10, 15, 20};
+
+        System.out.println(angka[0] + angka[3]);
+        System.out.println(angka[1] + angka[2]);
+        System.out.println(angka[2] + angka[1]);
+        System.out.println(angka[3] + angka[0]);
+    }
+}
