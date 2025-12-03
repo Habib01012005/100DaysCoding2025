3447677b7b6e9300395f390c2f51f360b0a534a6
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-12-03 22:24:27 +0800

    Create Day87_ElemenArrayForEach.java

---
+++ Day87_ElemenArrayForEach.java
@@ -0,0 +1,9 @@
+public class Day87_ElemenArrayForEach {
+    public static void main(String[] args) {
+        int [] angka = {10, 20, 30, 40};
+
+        for (int a : angka) {
+            System.out.println(a);
+        }
+    }
+}
