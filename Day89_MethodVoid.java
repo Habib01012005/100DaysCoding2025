a60b4e08921c6e032620b5074dc12e68e0929ccc
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-12-05 21:35:35 +0800

    Create Day89_MethodVoid.java

---
+++ Day89_MethodVoid.java
@@ -0,0 +1,11 @@
+public class Day89_MethodVoid {
+    public static void Identitas(){
+        System.out.println("====Identitas====");
+        System.out.println("Nama  : Habib");
+        System.out.println("NIM   : D0223017");
+        System.out.println("Kelas : B23");
+    }
+    public static void main(String[] args) {
+        Identitas();
+    }
+}
