4e01c2c2d0f6e5628f8b56e150a6e480ca8cf69d
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-12-06 21:49:58 +0800

    Implement non-void method to add two integers

---
+++ Day90_MehodNonVoid.java
@@ -0,0 +1,9 @@
+public class Day90_MehodNonVoid {
+    public static int tambah(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int hasil = tambah(37, 19);
+        System.out.println(hasil);
+    }
+}
