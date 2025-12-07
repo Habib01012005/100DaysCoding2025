ce0cf6f747f395c0e43e3a914833c5706892b05e
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-12-07 22:32:44 +0800

    Create Day91_ParameterMethod.java

---
+++ Day91_ParameterMethod.java
@@ -0,0 +1,9 @@
+public class Day91_ParameterMethod {
+    public static int kali(int a, int b){
+        return a * b;
+    }
+    public static void main(String[] args) {
+        int hasil = kali(4, 8);
+        System.out.println(hasil);
+    }
+}
