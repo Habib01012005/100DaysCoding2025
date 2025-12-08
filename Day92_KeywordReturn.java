b6e77a7ca118b550f9c3cdd5b007942168606834
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-12-08 22:06:45 +0800

    Create Day92_KeywordReturn.java

---
+++ Day92_KeywordReturn.java
@@ -0,0 +1,8 @@
+public class Day92_KeywordReturn {
+    public static String Nama(String nama){
+        return nama;
+    }
+    public static void main(String[] args) {
+        System.out.println(Nama("Habib"));
+    }
+}
