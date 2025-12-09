9c4ddf7a2e0f5988894f996adfd3939f185f208a
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-12-09 21:13:36 +0800

    Create Day94_MethodBilanganBulat.java

---
+++ Day94_MethodBilanganBulat.java
@@ -0,0 +1,9 @@
+public class Day94_MethodBilanganBulat {
+    public static void main(String[] args) {
+        double angka = 5.6;
+
+        System.out.println(Math.ceil(angka));
+        System.out.println(Math.floor(angka));
+        System.out.println(Math.round(angka));
+    }
+}
