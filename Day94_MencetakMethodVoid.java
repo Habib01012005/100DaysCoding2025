1f456d8c72acba26b49ff7974e8cfe9ede524701
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-12-10 22:45:41 +0800

    Create Day94_MencetakMethodVoid.java

---
+++ Day94_MencetakMethodVoid.java
@@ -0,0 +1,10 @@
+public class Day94_MencetakMethodVoid {
+    public static void Nama(){
+        String nama = "Habib";
+        String nim = "D0223017";
+        System.out.println(nama +" "+ nim);
+    }
+    public static void main(String[] args) {
+        Nama();
+    }
+}
