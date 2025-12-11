6bec5b3ba4a9e4aee80cb947b1773004b897a8ee
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-12-11 22:40:54 +0800

    Create Day95_CetakParameterVoid.java

---
+++ Day95_CetakParameterVoid.java
@@ -0,0 +1,8 @@
+public class Day95_CetakParameterVoid {
+    public static void Nama(String nama){
+        System.out.println("Nama Saya "+nama);
+    }
+    public static void main(String[] args) {
+        Nama("Habib");
+    }
+}
