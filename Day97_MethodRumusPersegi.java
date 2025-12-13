9c28879cbf8f26c1bd4e7f8d078d6e7908508aab
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-12-13 23:04:30 +0800

    Create Day97_MethodRumusPersegi.java

---
+++ Day97_MethodRumusPersegi.java
@@ -0,0 +1,9 @@
+public class Day97_MethodRumusPersegi {
+    public static void main(String[] args) {
+        System.out.println(persegi(5));
+    }
+    static int persegi(int s){
+        int luas = s * s;
+        return luas;
+    }
+}
